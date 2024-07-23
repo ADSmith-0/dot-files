57941111c1357ed67c8a56ec4d471ec85f648af8
adam smith <adam00199382818@gmail.com>
2024-07-23 11:49:40 +0100

    added script to install dependencies

---
+++ install_dependencies.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Telescope
+sudo apt install fzf ripgrep make cmake
+
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+exec zsh
+
+# neovim
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install neovim
+
+# tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
