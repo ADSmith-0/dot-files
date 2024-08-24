18dc9d4b5ffb5411245c59ef54eed05943f42d27
adam smith <adam00199382818@gmail.com>
2024-08-24 17:49:03 +0100

    removed comments and moved nvim tree to right side

--- nvim/lua/config-1/nvim-tree.lua
+++ nvim/lua/config-1/nvim-tree.lua
@@ -2,10 +2,9 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- empty setup using defaults
-require("nvim-tree").setup()
-
--- Enable for colour change on highlight
--- local color = require("onedarkpro.helpers")
--- local colors = color.get_colors()
--- vim.cmd('highlight NvimTreeCursorLine guifg=' .. colors.purple)
+require("nvim-tree").setup({
+  view = {
+    side = "right",
+    width = 45
+  }
+})
