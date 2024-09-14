cbc721c74087f14d0b9de6aa2d748121ea8734c1
ADSmith-0 <change me>
2024-09-14 14:05:22 +0100

    added .gitconfig to dot files

--- unpack.sh
+++ unpack.sh
@@ -4,4 +4,5 @@ ln -s $PWD/nvim ~/.config/nvim
 ln -s $PWD/tmux ~/.config/tmux
 ln -s $PWD/alacritty ~/.config/alacritty
 ln -s $PWD/.zshrc ~/.zshrc
+ln -s $PWD/.gitconfig ~/.gitconfig
 sudo ln -s $PWD/tmux-sessionizer /usr/bin/tmux-sessionizer
