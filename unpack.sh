472eb50506d42aba852f20f9f72afe3370449707
ADSmith-0 <change me>
2024-10-20 19:18:51 +0100

    added symlink for custom oh-my-zsh theme

--- unpack.sh
+++ unpack.sh
@@ -5,4 +5,5 @@ ln -s $PWD/tmux ~/.config/tmux
 ln -s $PWD/alacritty ~/.config/alacritty
 ln -s $PWD/.zshrc ~/.zshrc
 ln -s $PWD/.gitconfig ~/.gitconfig
+ln -s $PWD/adams.zsh-theme ~/.oh-my-zsh/themes/adams.zsh-theme
 sudo ln -s $PWD/tmux-sessionizer /usr/bin/tmux-sessionizer
