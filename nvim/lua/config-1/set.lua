07476a0612c6e534281d6110fddcc46956a6dcce
ADSmith-0 <adam00199382818@gmail.com>
2024-12-01 23:17:33 +0000

    fixed code indentation for github

--- nvim/lua/config-1/set.lua
+++ nvim/lua/config-1/set.lua
@@ -2,10 +2,10 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.autoindent = true
-vim.opt.smartindent = true
+vim.opt.softtabstop = 0
+vim.opt.shiftwidth = 0
+-- vim.opt.autoindent = true
+-- vim.opt.smartindent = true
 vim.opt.expandtab = true
 
 vim.opt.wrap = true
