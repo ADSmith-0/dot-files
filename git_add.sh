48cb0e205673de29275ef93d7a12c5c2e47efcf6
ADSmith-0 <adam00199382818@gmail.com>
2025-02-08 00:44:58 +0000

    changed keybind from ctrl-p to ctrl-a

--- git_add.sh
+++ git_add.sh
@@ -10,7 +10,7 @@ git_search='git ls-files \
 && eval "$git_search | fzf -m --ansi --preview 'git diff --color=always -- {+}' \
   --no-sort \
   --prompt='Add >' \
-  --bind='ctrl-p:execute(git add {+} -p)' \
-  --bind='ctrl-p:+reload($git_search)' \
+  --bind='ctrl-a:execute(git add {+} -p)' \
+  --bind='ctrl-a:+reload($git_search)' \
   --bind='enter:execute(git add {+})' \
   --bind='enter:+reload($git_search)'"
