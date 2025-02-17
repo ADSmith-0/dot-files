95eede9b2ad93711d67dea82b56bfa10954eb5fd
ADSmith-0 <adam00199382818@gmail.com>
2025-02-17 09:16:28 +0000

    added search across files for selected word as well

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -3,6 +3,7 @@ vim.keymap.set("n", "<leader>f", function() builtin.fd({ hidden = true, no_ignor
   { desc = "Find file" })
 vim.keymap.set("n", "<leader>w", builtin.live_grep, { desc = "Find word" })
 vim.keymap.set("n", "<leader>*", builtin.grep_string, { desc = "Find word under cursor" })
+vim.keymap.set("v", "<leader>*", builtin.grep_string, { desc = "Find selection in files" })
 vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Search open buffers" })
 vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Search git files" })
 vim.keymap.set("n", "<leader>;", builtin.resume, { desc = "Open last search" })
