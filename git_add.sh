a87ce428f131b3e38fb5f0b81097f96fcadac7e5
ADSmith-0 <adam00199382818@gmail.com>
2025-04-12 01:38:37 +0100

    changed git add script to exit after adding files

--- git_add.sh
+++ git_add.sh
@@ -9,8 +9,6 @@ git_search='git ls-files \
  $(git rev-parse --show-toplevel)' \
 && eval "$git_search | fzf -m --ansi --preview 'git diff --color=always -- {+}' \
   --no-sort \
-  --prompt='Add >' \
-  --bind='ctrl-a:execute(git add {+} -p)' \
-  --bind='ctrl-a:+reload($git_search)' \
-  --bind='enter:execute(git add {+})' \
-  --bind='enter:+reload($git_search)'"
+  --prompt='Add> ' \
+  --bind='ctrl-a:become(git add {+} -p)' \
+  --bind='enter:become(git add {+})'"
