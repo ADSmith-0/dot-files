6560953a4eaa2043432c80284c2b80f33253cb3c
ADSmith-0 <adam00199382818@gmail.com>
2025-09-27 17:45:20 +0100

    Updated dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Telescope
-sudo apt install fzf ripgrep make
+sudo apt install ripgrep make
 
 # nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 exec zsh
+nvm install node
 
 # neovim
 sudo add-apt-repository ppa:neovim-ppa/unstable
@@ -13,6 +14,9 @@ sudo apt-get install neovim
 
 #tmux
 sudo apt install tmux
+
+# zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
