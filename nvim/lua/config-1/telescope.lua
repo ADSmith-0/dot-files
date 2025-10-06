464e05a8db973174c17d09ecdef26eab0d618432
ADSmith-0 <adam00199382818@gmail.com>
2025-10-07 00:46:28 +0100

    changed default style for telescope

--- nvim/lua/config-1/telescope.lua
+++ nvim/lua/config-1/telescope.lua
@@ -1,9 +1,19 @@
 require('telescope').setup {
   defaults = {
+    color_devicons = true,
+    sorting_strategy = "ascending",
+    path_displays = "smart",
+    layout_strategy = "horizontal",
+    layout_config = {
+      prompt_position = "top",
+      preview_width = 0.70,
+      height = 0.99,
+      width = 0.99,
+    },
     file_ignore_patterns = {
       ".git",
       "node_modules"
-    }
+    },
   }
 }
 require('telescope').load_extension('fzf')
