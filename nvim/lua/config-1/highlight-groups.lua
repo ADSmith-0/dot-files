dabe137cf749cde8c05d900901e084f3308aa7ed
ADSmith-0 <adam00199382818@gmail.com>
2025-11-07 20:12:42 +0000

    Added code comments

--- nvim/lua/config-1/highlight-groups.lua
+++ nvim/lua/config-1/highlight-groups.lua
@@ -1,4 +1,7 @@
+-- Make Oil UI the same colour as nvim background
 vim.cmd([[hi! link NormalFloat Normal]])
 vim.cmd([[hi! link FloatBorder Normal]])
+
+-- Make Telescope UI the same colour as nvim background
 vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
 vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
