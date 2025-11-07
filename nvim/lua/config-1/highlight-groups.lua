c4440e26af7dddcd740f3d293ae2743a22c543e3
ADSmith-0 <adam00199382818@gmail.com>
2025-11-07 20:10:48 +0000

    Renamed hightlight-groups to highlight-groups

--- nvim/lua/config-1/highlight-groups.lua
+++ nvim/lua/config-1/highlight-groups.lua
@@ -1,3 +1,4 @@
 vim.cmd([[hi! link NormalFloat Normal]])
 vim.cmd([[hi! link FloatBorder Normal]])
 vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
