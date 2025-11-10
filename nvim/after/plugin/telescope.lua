887992047d5c54e04a4ab3a6f4c83352cf319d14
ADSmith-0 <adam00199382818@gmail.com>
2025-11-10 22:36:33 +0000

    Updated telescope config

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,9 +1,12 @@
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>f", function() builtin.fd({ hidden = true, no_ignore = true, no_ignore_parent = true }) end,
+vim.keymap.set("n", "<leader>f",
+  function() builtin.fd({ hidden = true, no_ignore = true, no_ignore_parent = true, previewer = false, layout_config = { height = 0.75, width = 0.65 } }) end,
   { desc = "Find file" })
 vim.keymap.set("n", "<leader>w", builtin.live_grep, { desc = "Find word" })
 vim.keymap.set("n", "<leader>*", builtin.grep_string, { desc = "Find word under cursor" })
 vim.keymap.set("v", "<leader>*", builtin.grep_string, { desc = "Find selection in files" })
-vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Search open buffers" })
+vim.keymap.set("n", "<leader><leader>",
+  function() builtin.buffers({ hidden = true, no_ignore = true, no_ignore_parent = true, previewer = false, layout_config = { height = 0.75, width = 0.65 } }) end,
+  { desc = "Search open buffers" })
 vim.keymap.set("n", "<leader>gf", builtin.git_files, { desc = "Search git files" })
 vim.keymap.set("n", "<leader>;", builtin.resume, { desc = "Open last search" })
--- nvim/lua/config-1/telescope.lua
+++ nvim/lua/config-1/telescope.lua
@@ -2,8 +2,10 @@ require('telescope').setup {
   defaults = {
     color_devicons = true,
     sorting_strategy = "ascending",
-    path_displays = "truncate",
+    path_display = { "smart" },
     layout_strategy = "horizontal",
+    prompt_prefix = " 🔍 ",
+    selection_caret = ">  ",
     layout_config = {
       prompt_position = "top",
       preview_width = 0.70,
