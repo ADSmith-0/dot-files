ef16e8e40715911cf22604bb10176b36759c98c9
ADSmith-0 <adam00199382818@gmail.com>
2025-12-20 01:20:29 +0000

    Added git-switch script

---
+++ git-switch.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [[ $# -eq 1 ]]; then
+    selected=$1
+else
+    selected=$(git branch --list | fzf | xargs)
+fi
+
+if [[ $selected =~ "-" ]]; then
+  git switch -
+  exit 0
+fi
+
+if git show-ref --quiet --branches $selected; then
+  git switch "$selected"
+else
+  git switch -c "$selected"
+fi
+
