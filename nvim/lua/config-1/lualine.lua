64e582ea1b97e4382392757386989c776a1fddc4
ADSmith-0 <adam00199382818@gmail.com>
2026-01-07 09:28:59 +0000

    Changed lualine style

--- nvim/lua/config-1/lualine.lua
+++ nvim/lua/config-1/lualine.lua
@@ -1,1 +1,13 @@
-require("lualine").setup()
+require("lualine").setup({
+  options = {
+    component_separators = { left = '|', right = '|' },
+  },
+  sections = {
+    lualine_a = { { 'mode', separator = { left = '' } } },
+    lualine_b = {},
+    lualine_c = { { 'diff', icon = { '', color = { fg = 'lightgreen' } }, separator = { right = '' } }, 'diagnostics' },
+    lualine_x = { 'location', 'progress', 'lsp_status', 'filetype', 'encoding', { 'fileformat', separator = { right = '' } }, },
+    lualine_y = {},
+    lualine_z = {}
+  },
+})
