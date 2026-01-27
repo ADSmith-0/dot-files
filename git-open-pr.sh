442b618c9b3b66bace57d1a37962739f7539e62b
ADSmith-0 <adam00199382818@gmail.com>
2026-01-27 00:54:49 +0000

    Added command to open pr from command line

---
+++ git-open-pr.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+no_pr=$(gh pr view | tr -cd "[:print:]\n")
+
+if [[ -z $no_pr ]]; then
+  url=$(git remote get-url origin | sed -E "s|:|/|" | sed -E "s|git@|https://www.|" | sed -E "s|\.git$||")
+  branch=$(git rev-parse --abbrev-ref HEAD)
+  open "$url/compare/$branch?expanded=1"
+else
+  gh pr view --web
+fi
--- unpack.sh
+++ unpack.sh
@@ -10,5 +10,6 @@ ln -s $PWD/adams.zsh-theme ~/.oh-my-zsh/themes/adams.zsh-theme
 sudo ln -s $PWD/tmux-sessionizer /usr/bin/tmux-sessionizer
 sudo ln -s $PWD/git-add.sh /usr/bin/git-add.sh
 sudo ln -s $PWD/git-sync.sh /usr/bin/git-sync.sh
-sudo ln -s $PWD/git-open-remote.sh /usr/bin/git-open-remote.sh
 sudo ln -s $PWD/git-switch.sh /usr/bin/git-switch.sh
+sudo ln -s $PWD/git-open-remote.sh /usr/bin/git-open-remote.sh
+sudo ln -s $PWD/git-open-pr.sh /usr/bin/git-open-pr.sh
