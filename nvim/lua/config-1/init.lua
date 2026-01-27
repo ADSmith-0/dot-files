195c41fc8b29c188e32bd16a8a457c772c943cea
ADSmith-0 <adam00199382818@gmail.com>
2026-01-27 17:50:39 +0000

    Updated leap.nvim repo

--- nvim/lua/config-1/init.lua
+++ nvim/lua/config-1/init.lua
@@ -132,7 +132,7 @@ local plugins = {
       })
     end,
   },
-  { 'nvim-mini/mini.pairs', version = '*' },
+  { 'nvim-mini/mini.pairs',      version = '*' },
   {
     "kylechui/nvim-surround",
     version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
@@ -157,7 +157,7 @@ local plugins = {
     event = "VeryLazy"
   },
   {
-    "ggandor/leap.nvim",
+    url = "https://codeberg.org/andyg/leap.nvim",
     enabled = true,
   },
   {
