34ce387bf2c1e0ae8c43ab89429943288a5c4892
Adam Smith <32732098+ADSmith-0@users.noreply.github.com>
2026-02-12 22:54:29 +0000

    #28 Add in conform.nvim

---
+++ nvim/lua/config-1/conform.lua
@@ -0,0 +1,12 @@
+require("conform").setup({
+  formatters_by_ft = {
+    lua = { "stylua" },
+    -- Conform will run multiple formatters sequentially
+    python = { "isort", "black" },
+    -- You can customize some of the format options for the filetype (:help conform.format)
+    rust = { "rustfmt", lsp_format = "fallback" },
+    go = { "gopls" },
+    -- Conform will run the first available formatter
+    javascript = { "biome", "prettierd", "prettier", stop_after_first = true },
+  },
+})
