60626e81b08361c9867215293e85fe8ccc06b34e
ADSmith-0 <adam00199382818@gmail.com>
2026-02-16 23:05:26 +0000

    Fixed issue where autocmds would run on telescope prompt

--- nvim/lua/config-1/autocmd.lua
+++ nvim/lua/config-1/autocmd.lua
@@ -2,13 +2,19 @@
 -- vim.cmd([[autocmd BufWritePost * lua vim.lsp.buf.format()]])
 vim.api.nvim_create_autocmd("InsertEnter", {
   desc = "Disable relative line numbers",
-  callback = function()
+  callback = function(opts)
+    if vim.bo[opts.buf].filetype == 'TelescopePrompt' or vim.bo[opts.buf].filetype == 'TelescopeResults' then
+      return
+    end
     vim.opt.relativenumber = false
   end
 })
 vim.api.nvim_create_autocmd("InsertLeave", {
   desc = "Enable relative line numbers",
-  callback = function()
+  callback = function(opts)
+    if vim.bo[opts.buf].filetype == 'TelescopePrompt' or vim.bo[opts.buf].filetype == 'TelescopeResults' then
+      return
+    end
     vim.opt.relativenumber = true
   end
 })
