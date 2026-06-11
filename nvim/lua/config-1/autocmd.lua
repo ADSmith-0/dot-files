58db094c5ca5e4ae894ce828d467d97e01813637
ADSmith-0 <adam00199382818@gmail.com>
2026-06-11 22:54:58 +0100

    Removed relative line numbers changing

--- nvim/lua/config-1/autocmd.lua
+++ nvim/lua/config-1/autocmd.lua
@@ -1,15 +1,24 @@
 -- Auto format on save
 -- vim.cmd([[autocmd BufWritePost * lua vim.lsp.buf.format()]])
-vim.api.nvim_create_autocmd("InsertEnter", {
-  desc = "Disable relative line numbers",
-  callback = function(opts)
-    if vim.bo[opts.buf].filetype == 'TelescopePrompt' or vim.bo[opts.buf].filetype == 'TelescopeResults' then
-      return
-    end
-    vim.opt.relativenumber = false
-  end
-})
-vim.api.nvim_create_autocmd("InsertLeave", {
+-- vim.api.nvim_create_autocmd("InsertEnter", {
+--   desc = "Disable relative line numbers",
+--   callback = function(opts)
+--     if vim.bo[opts.buf].filetype == 'TelescopePrompt' or vim.bo[opts.buf].filetype == 'TelescopeResults' then
+--       return
+--     end
+--     vim.opt.relativenumber = false
+--   end
+-- })
+-- vim.api.nvim_create_autocmd("InsertLeave", {
+--   desc = "Enable relative line numbers",
+--   callback = function(opts)
+--     if vim.bo[opts.buf].filetype == 'TelescopePrompt' or vim.bo[opts.buf].filetype == 'TelescopeResults' then
+--       return
+--     end
+--     vim.opt.relativenumber = true
+--   end
+-- })
+vim.api.nvim_create_autocmd("BufEnter", {
   desc = "Enable relative line numbers",
   callback = function(opts)
     if vim.bo[opts.buf].filetype == 'TelescopePrompt' or vim.bo[opts.buf].filetype == 'TelescopeResults' then
