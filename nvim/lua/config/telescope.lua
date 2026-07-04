b3fc35f2202fafb21c02e3db1ec0c849a625d89d
ADSmith-0 <change me>
2026-07-04 15:12:19 +0100

    Fixed Telescope config

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -1,6 +1,45 @@
-require("telescope").setup()
+require('telescope').setup {
+  defaults = {
+    color_devicons = true,
+    sorting_strategy = "ascending",
+    path_display = { "smart" },
+    layout_strategy = "horizontal",
+    prompt_prefix = " 🔍 ",
+    selection_caret = "▋ ",
+    layout_config = {
+      prompt_position = "top",
+      height = 0.99,
+      width = 0.99,
+      horizontal = {
+        preview_width = 0.70
+      }
+    },
+    file_ignore_patterns = {
+      ".git",
+      "node_modules",
+      "android",
+      "ios"
+    },
+  }
+}
+
+require('telescope').load_extension('fzf')
 
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Telescope find files" })
-vim.keymap.set("n", "<leader>w", builtin.live_grep, { desc = "Telescope live grep" })
-vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>f",
+  function() builtin.fd({ hidden = true, no_ignore = true, no_ignore_parent = true, previewer = false, layout_config = { height = 0.75, width = 0.65 } }) end,
+  { desc = "Find file" })
+vim.keymap.set("n", "<leader>w", builtin.live_grep, { desc = "Find word" })
+vim.keymap.set("n", "<leader>*", builtin.grep_string, { desc = "Find word under cursor" })
+vim.keymap.set("v", "<leader>*", builtin.grep_string, { desc = "Find selection in files" })
+vim.keymap.set("n", "<leader><leader>",
+  function() builtin.buffers({ hidden = true, no_ignore = true, no_ignore_parent = true, previewer = false, layout_config = { height = 0.75, width = 0.65 } }) end,
+  { desc = "Search open buffers" })
+vim.keymap.set("n", "<leader>;", builtin.resume, { desc = "Open last search" })
+vim.keymap.set("n", "<leader>gx", function ()
+  builtin.git_branches({ pattern = "refs/heads" })
+end,  { desc = "Switch git branch" })
+vim.keymap.set("n", "<leader>gl", builtin.git_commits, { desc = "Show git commit history" })
+vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Get git status" })
+vim.keymap.set("n", "<leader>/", function() builtin.current_buffer_fuzzy_find({ skip_empty_lines = true }) end,
+  { desc = "Search buffer" })
