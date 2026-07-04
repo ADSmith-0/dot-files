b9daa7355b13b1451b978fedc47bfa01a643e1a0
ADSmith-0 <change me>
2026-07-04 15:23:21 +0100

    Added tmux-vim-navigation

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -17,5 +17,24 @@ return {
 	},
 	{ "folke/todo-comments.nvim", opts = {} },
 	{ "nvim-lua/plenary.nvim", lazy = true },
+	{
+	  "christoomey/vim-tmux-navigator",
+	  cmd = {
+	    "TmuxNavigateLeft",
+	    "TmuxNavigateDown",
+	    "TmuxNavigateUp",
+	    "TmuxNavigateRight",
+	    "TmuxNavigatePrevious",
+	    "TmuxNavigatorProcessList",
+	  },
+	  keys = {
+	    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+	    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+	    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+	    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+	    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+	  },
+	  lazy = true
+	},
 	{ "olimorris/onedarkpro.nvim", lazy = true }
 } 
