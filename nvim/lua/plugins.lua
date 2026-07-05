a638c858be80d8f144d5aaf3fdf997de198b23dc
ADSmith-0 <adam00199382818@gmail.com>
2026-07-05 23:11:20 +0100

    Added trouble.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -134,4 +134,42 @@ return {
 	-- { "mbbill/undotree", lazy = true },
 	{ "akinsho/git-conflict.nvim", version = "*", config = true },
 	{ "nvim-mini/mini.indentscope", version = "*", lazy = true },
+	{
+		"folke/trouble.nvim",
+		lazy = true,
+		opts = {},
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>x",
+				"<cmd>Trouble diagnostics toggle filter.buf=0 win.position=right<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>X",
+				"<cmd>Trouble diagnostics toggle win.position=right<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			-- {
+			-- 	"<leader>cs",
+			-- 	"<cmd>Trouble symbols toggle focus=false<cr>",
+			-- 	desc = "Symbols (Trouble)",
+			-- },
+			-- {
+			-- 	"<leader>cl",
+			-- 	"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			-- 	desc = "LSP Definitions / references / ... (Trouble)",
+			-- },
+			-- {
+			-- 	"<leader>xL",
+			-- 	"<cmd>Trouble loclist toggle<cr>",
+			-- 	desc = "Location List (Trouble)",
+			-- },
+			-- {
+			-- 	"<leader>xQ",
+			-- 	"<cmd>Trouble qflist toggle<cr>",
+			-- 	desc = "Quickfix List (Trouble)",
+			-- },
+		},
+	},
 }
