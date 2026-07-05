c9348e3acb8116541ed4c6c09b4c6c80012f8480
ADSmith-0 <adam00199382818@gmail.com>
2026-07-06 00:12:07 +0100

    Added undercurl to nvim

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -1,9 +1,17 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+vim.opt.termguicolors = true
+
+vim.o.winborder = "rounded"
+
+-- auto-session
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- Make sign column always visible (no CLS when signs appear/disappear)
 vim.opt.signcolumn = "yes"
 
 vim.opt.wrap = true
@@ -15,19 +23,17 @@ vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+-- Enable undercurl
+vim.api.nvim_set_var("t_Cs", "\\e[4:3m")
+vim.api.nvim_set_var("t_Ce", "\\e[4:0m")
+
+-- Set tab width
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = false
 
 vim.opt.scrolloff = 4
-
-vim.opt.termguicolors = true
-
-vim.o.winborder = "rounded"
-
--- auto-session
-vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
 -- if vim.opt.diff:get() then
 --  vim.o.diffopt = 'internal,filler,closeoff'
