3e0ca689686fab7d0e29b6290b3615565e27647b
ADSmith-0 <adam00199382818@gmail.com>
2026-07-20 01:12:16 +0100

    Updated packages and fixed issues with conform

--- nvim/lua/config/conform.lua
+++ nvim/lua/config/conform.lua
@@ -2,6 +2,8 @@ require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
 		rust = { "rustfmt", lsp_format = "fallback" },
-		javascript = { "biome", "prettierd", "prettier", stop_after_first = true },
+		javascript = { "biome", "prettier", stop_after_first = false },
+		typescript = { "biome", "prettier", stop_after_first = false },
+		svelte = { "biome", "prettier", stop_after_first = false },
 	},
 })
